81abf2c2a554c1122c114c1bb1ce5a25d0e638fe
avery <averywald.dev@gmail.com>
2021-04-19 19:42:29 -0500

    made draggable textblock

--- scribl/MainWindow.xaml.cs
+++ scribl/MainWindow.xaml.cs
@@ -20,9 +20,37 @@ namespace scribl
     /// </summary>
     public partial class MainWindow : Window
     {
+        Nullable<Point> dragStart = null; // will hold coordinates on drag event
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // example textbox
+            // TextBox test = new TextBox();
+            TextBlock test = new TextBlock();
+            test.Text = "hello, i am a text box";
+            test.MouseMove += OnMouseMove;
+
+            mainCanvas.Children.Add(test);
+        }
+
+        void OnMouseMove(object sender, MouseEventArgs args)
+        {
+            if (args.Source is TextBlock tb)
+            {
+                if (args.LeftButton == MouseButtonState.Pressed)
+                {
+                    Point p = args.GetPosition(mainCanvas);
+                    Canvas.SetLeft(tb, p.X - tb.ActualWidth / 2);
+                    Canvas.SetTop(tb, p.Y - tb.ActualHeight / 2);
+                    tb.CaptureMouse();
+                }
+                else
+                {
+                    tb.ReleaseMouseCapture();
+                }
+            }
         }
     }
 }
